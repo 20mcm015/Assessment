33494d20fe234224e8d4e0f6dbdc35232b788c9d
Karthikeyan <104311444+20mcm015@users.noreply.github.com>
2022-10-14 09:30:12 +0530

    Update Signin.java

--- src/mobileworld/Signin.java
+++ src/mobileworld/Signin.java
@@ -1,70 +1,120 @@
 package mobileworld;
-
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-
-public class Signin {
-@Test(priority=1)
-
-	public void sin1() throws InterruptedException {
-		// TODO Auto-generated method stub
-		System.setProperty("webdriver.chrome.driver", "C:/Users/karthikeyan.s/Downloads/chromedriver_win32/chromedriver.exe");
-		WebDriver driver =  new ChromeDriver();
-		Landingpage landingpage = new Landingpage(driver);
-		landingpage.goTo();
-		landingpage.loginApplication("karthik", "karthik123");
-		driver.findElement(By.xpath("//a[@type='submit']")).click();
-		
-		
-
-	}
-
-@Test(priority=2)
-//max 20 characters in username textfeild and max 10 characters in password testfeild
-public void sin2() throws InterruptedException {
-	// TODO Auto-generated method stub
-	System.setProperty("webdriver.chrome.driver", "C:/Users/karthikeyan.s/Downloads/chromedriver_win32/chromedriver.exe");
-	WebDriver driver =  new ChromeDriver();
-	Landingpage landingpage = new Landingpage(driver);
-	landingpage.goTo();
-	landingpage.loginApplication("karthikeyankarthikeyankarthikeyan", "karthikeyan123");
-	driver.findElement(By.xpath("//a[@type='submit']")).click();
-	
-
-}
-
-@Test(priority=3)
-//less than 3 characters in username textfeild and less than 3 characters in password testfeild
-public void sin3() throws InterruptedException {
-	// TODO Auto-generated method stub
-	System.setProperty("webdriver.chrome.driver", "C:/Users/karthikeyan.s/Downloads/chromedriver_win32/chromedriver.exe");
-	WebDriver driver =  new ChromeDriver();
-	Landingpage landingpage = new Landingpage(driver);
-	landingpage.goTo();
-	landingpage.loginApplication("ka", "k1");
-	driver.findElement(By.xpath("//a[@type='submit']")).click();
+public class Sign_inScript {
+   
+	@Test(dataProvider="Signin") 
+    public void signin(String username,String Password) throws Exception
+    {
 
 
-}
-@Test(priority=4)
-//without data
-public void sin4() throws InterruptedException {
-	// TODO Auto-generated method stub
-	System.setProperty("webdriver.chrome.driver", "C:/Users/karthikeyan.s/Downloads/chromedriver_win32/chromedriver.exe");
-	WebDriver driver =  new ChromeDriver();
-	Landingpage landingpage = new Landingpage(driver);
-	landingpage.goTo();
-	landingpage.loginApplication("", "");
-	driver.findElement(By.xpath("//a[@type='submit']")).click();
-	
 
-}
+   
+        System.setProperty("webdriver.chrome.driver","C:/Users/karthikeyan.s/Downloads/chromedriver_win32/chromedriver.exe");
+         WebDriver driver = new ChromeDriver();
+          
+            Signinpom page=new Signinpom(driver);
+        
+            page.goTo();
+            driver.manage().window().maximize();
+            page.signinbutton().click();
+            System.out.println("click the signin button");
+            Thread.sleep(2000);
+            WebElement user_name = driver.findElement(By.xpath("//label[@for='username']"));
+        	user_name.isDisplayed();
+            System.out.println("enter the username");
+            page.uname().sendKeys(username);
+            Thread.sleep(2000);
+            WebElement Pass = driver.findElement(By.xpath("//label[@for='password']"));
+        	Pass.isDisplayed();
+            System.out.println("enter the password");
+            page.pwd().sendKeys(Password);
+            Thread.sleep(2000);
+            WebElement Remember_me = driver.findElement(By.xpath("//label[@for='remember-me']"));
+            Remember_me.isDisplayed();
+            Thread.sleep(2000);
+            page.Remember_click.isDisplayed();
+            page.Remember_click.click();
+        
+            driver.findElement(By.xpath("//a[@type='submit']")).click();
+            driver.close();
+            int uname=username.length();
+            int pass=Password.length(); 
+            
+            
+            //Username
+            
+            if ((uname <= 3) || (uname >= 20)) {
+            	
+            	Assert.assertEquals(true, uname);
+            	System.out.println("Invalid Username");
+            	
+            }
+           
+           
+            else {
+            	System.out.println("Valid Username");
+            }
+              
+         
 
-	
-			
-	
+           //password 
+            
+           if ((pass <= 3) || (pass >= 10)) {
+           Assert.assertEquals(true, pass);
+           System.out.println("Inalid password");
+           
+           }
+           else {
+        	   System.out.println("Valid password");
+           }
+           
+          
+          
+            
+            driver.quit();
+        }
+    @DataProvider(name="Signin")
+    public Object[][] getdata()
+    {
+      Object [][] data=new Object[6][2];
+      
+      
+      //username and password - vaild data
+      data [0][0]="karthik";
+      data [0][1]="karthik@1";
+      
+      //username and password - less than 3 characters
+      data [1][0]="ka";
+      data [1][1]="k@";
+      
+      
+      //username and password - greater than 10 characters
+      data [2][0]="Karthikeyansk";
+      data [2][1]="karthikeyan@12345.";
+      
+      
+      //email and password - vaild data
+      
+      data [3][0]="karthik@gmail.com";
+      data [3][1]="karthik@1";
+      
+      
+      //invalid email id
+      data [4][0]="karthikeyan.gmail.com";
+      data [4][1]="karthik@1";
+      
+      
+      //without data
+      data [5][0]="";
+      data [5][1]="";
+      
+      return data;
+        
+    }
 }
